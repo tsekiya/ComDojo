924100d18a0b543a94855633d9a5a1eefa021165
Sekiya Takeshi <18338637+tsekiya@users.noreply.github.com>
2018-11-12 16:32:02 +0900

    override キーワードを追加

--- COM Exception/WinrtTemperature/ComDojoErrorInfo.h
+++ COM Exception/WinrtTemperature/ComDojoErrorInfo.h
@@ -7,55 +7,55 @@ struct ComDojoErrorInfo : winrt::implements<::ComDojoErrorInfo, ::IComDojoErrorI
 	// ICreateErrorInfo
 
 	// エラーが発生したIIDを設定
-	STDMETHOD(SetGUID)(/* [in] */  REFGUID rguid) noexcept ;
+	STDMETHOD(SetGUID)(/* [in] */  REFGUID rguid) noexcept override;
 
 	// エラーが発生したCLSIDを設定
-	STDMETHOD(SetSource)(/* [in] */  LPOLESTR szSource) noexcept ;
+	STDMETHOD(SetSource)(/* [in] */  LPOLESTR szSource) noexcept override;
 
 	// エラーの詳細
-	STDMETHOD(SetDescription)(/* [in] */  LPOLESTR szDescription) noexcept ;
+	STDMETHOD(SetDescription)(/* [in] */  LPOLESTR szDescription) noexcept override;
 
 	// ヘルプファイルへのパス
-	STDMETHOD(SetHelpFile)(/* [in] */  LPOLESTR szHelpFile) noexcept ;
+	STDMETHOD(SetHelpFile)(/* [in] */  LPOLESTR szHelpFile) noexcept override;
 
 	// ヘルプのコンテキスト
-	STDMETHOD(SetHelpContext)(/* [in] */ DWORD dwHelpContext) noexcept ;
+	STDMETHOD(SetHelpContext)(/* [in] */ DWORD dwHelpContext) noexcept override;
 
 
 	// IErrorInfo
 
 	// エラーが発生したIIDを取得
-	STDMETHOD(GetGUID)(/* [out] */  GUID *pGUID) noexcept ;
+	STDMETHOD(GetGUID)(/* [out] */  GUID *pGUID) noexcept override;
 
 	// エラーが発生したCLSIDを取得
-	STDMETHOD(GetSource)(/* [out] */  BSTR *pBstrSource) noexcept ;
+	STDMETHOD(GetSource)(/* [out] */  BSTR *pBstrSource) noexcept override;
 
 	// エラーの詳細
-	STDMETHOD(GetDescription)(/* [out] */  BSTR *pBstrDescription) noexcept ;
+	STDMETHOD(GetDescription)(/* [out] */  BSTR *pBstrDescription) noexcept override;
 
 	// ヘルプファイルへのパス
-	STDMETHOD(GetHelpFile)(/* [out] */  BSTR *pBstrHelpFile) noexcept ;
+	STDMETHOD(GetHelpFile)(/* [out] */  BSTR *pBstrHelpFile) noexcept override;
 
 	// ヘルプのコンテキスト
-	STDMETHOD(GetHelpContext)(/* [out] */  DWORD *pdwHelpContext) noexcept ;
+	STDMETHOD(GetHelpContext)(/* [out] */  DWORD *pdwHelpContext) noexcept override;
 
 	// IComDojoErrorInfo
 
 	// エラーコードを取得する
-	STDMETHOD(GetErrorCode)(long* errorCode) noexcept ;
+	STDMETHOD(GetErrorCode)(long* errorCode) noexcept override;
 
 	// ICreateComDojoErrorInfo
 
 	// エラーコードを取得する
-	STDMETHOD(SetErrorCode)(long errorCode) noexcept ;
+	STDMETHOD(SetErrorCode)(long errorCode) noexcept override;
 
 	// IClassFactory
 
 	// インスタンスを返す
-	STDMETHOD(CreateInstance)(IUnknown* outer, IID const& iid, void** ppv) noexcept ;
+	STDMETHOD(CreateInstance)(IUnknown* outer, IID const& iid, void** ppv) noexcept override;
 
 	// COMサーバのロックをインクリメント、またはデクリメントする
-	STDMETHOD(LockServer)(BOOL lock) noexcept ;
+	STDMETHOD(LockServer)(BOOL lock) noexcept override;
 
 private:
 	GUID _iid;
